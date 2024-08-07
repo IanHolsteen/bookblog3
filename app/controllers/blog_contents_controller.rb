ab3b2c6e6213670d6cd0c906ef278e890a7a1651
IanHolsteen <ianholsteen@gmail.com>
2024-08-07 06:59:11 -0400

    finding and showing data

--- app/controllers/blog_contents_controller.rb
+++ app/controllers/blog_contents_controller.rb
@@ -1,7 +1,12 @@
 class BlogContentsController < ApplicationController
-    # skip_before_action :authorize, only: [:show, :update, :destroy, :index]
+    skip_before_action :authorize, only: [:show, :index]
 
     def index
         render json: BlogContent.all
     end
+
+    def show
+        render json: @blog
+    end
+
 end
---
+++ client/pages/blog.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Blog() {
+
+    const [blog, setBlog] = useState("")
+
+    useEffect(() => {
+        fetch("/api/blog").then((r) => {
+            if (r.ok) {r.json().then((data) => {
+              setBlog(data)});
+            }});
+        }, [setBlog]);
+    
+    return (
+        <div>
+            <p>Title: {blog[0]?.title}</p>
+            <p>Body: {blog[0].body}</p>
+        </div>
+    )
+}
